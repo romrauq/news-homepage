432f15cf4735393ffd560f27406242624f0bef31
romrauq <johnequarmor@gmail.com>
2025-02-05 07:30:38 +0000

    Removed scriipt that modifies the display of the side menu container.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -6,13 +6,13 @@ const close_menu = document.getElementById("close-menu-icon");
 // When open menu icon is clicked:
 open_menu.addEventListener("click", () => {
 	overlay.style.display = "block";
-	links_container.style.display = "flex";
+	// links_container.style.display = "flex";
 	links_container.style.right = "0";
 });
 
 // When close menu icon is clicked:
 close_menu.addEventListener("click", () => {
 	overlay.style.display = "none";
-	links_container.style.display = "none";
 	links_container.style.right = "-70vw";
+	// links_container.style.display = "none";
 });
